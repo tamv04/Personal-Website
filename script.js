8e8e33d10e1da0cd2c90b20b4057daaca70ae700
Tam Vo <130496541+tamv04@users.noreply.github.com>
2024-08-08 09:42:39 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+// header scrolling effect
+$(window).on('scroll', function(){
+	if($(window).scrollTop()){
+      $('header').addClass('nav-show');
+		  
+	} 
+	else{
+		$('header').removeClass('nav-show');
+	}
+	   
+})
+
+//hamburger
+const navSlide = () => {
+	 const hamburger = document.querySelector(".hamburger");
+	 const navbar = document.querySelector(".nav-bar");
+	 const navLinks = document.querySelectorAll(".nav-bar li");
+
+     hamburger.onclick = () => {
+		
+	 navbar.classList.toggle("nav-active");
+		 
+      //Animation links
+	 navLinks.forEach((link, index) => {
+		if (link.style.animation) {
+			link.style.animation = "";
+		} else {
+			link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7+1}s`;
+		   }
+		});
+	  //hamburger animation
+	 hamburger.classList.toggle("toggle");
+    }
+	 
+	}
+
+window.onload = () => navSlide();
+
+// Smooth scrolling for navigation links
+$('a.nav-link').on('click', function (event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top,
+      },
+      800, // Adjust the scroll speed as needed (in milliseconds)
+      function () {
+        window.location.hash = hash;
+      }
+    );
+  }
+});
